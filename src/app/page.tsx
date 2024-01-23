1f99ddf670b50c3319f020d2dcdb8922dac6a552
Olya <katniss30051991@gmail.com>
2024-01-23 14:16:15 +0200

    Added server and client componets and modal for adding company

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
-import StatusLabel, { Status } from './components/status-label';
+import AddCompanyButton from './components/add-company-button';
+import ClientComponent from './components/client-component';
+import ServerComponent from './components/server-component';
+import ServerComponentCopy from './components/server-component-copy';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive} disable={true}>
-        Not Active
-      </StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <AddCompanyButton />
+      <ServerComponent />
+      <ClientComponent>
+        <ServerComponentCopy />
+      </ClientComponent>
     </main>
   );
 }
