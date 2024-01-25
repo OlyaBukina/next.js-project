811714f949be2297297bd6dc45974ad212e0d53e
Olya <katniss30051991@gmail.com>
2024-01-25 18:54:41 +0200

    Finished course lessons

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import Header from '@/app/components/header';
-import Toolbar from '@/app/components/toolbar';
-import AddCompanyButton from '@/app/components/add-company-button';
-import SearchInput from '@/app/components/search-input';
 import CompanyTable from '@/app/components/company-table';
 import CompanyRow from '@/app/components/company-row';
 import { Status } from '@/app/components/status-label';
@@ -12,10 +8,6 @@ export interface PageProps {}
 export default function Page() {
   return (
     <>
-      <Header>Companies</Header>
-      <Toolbar action={<AddCompanyButton />}>
-        <SearchInput />
-      </Toolbar>
       <CompanyTable>
         <CompanyRow
           id={1}
